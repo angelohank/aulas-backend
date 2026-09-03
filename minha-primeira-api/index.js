20be3a006e0cf10bc8bc3e0ff1895da4bcf59ae7
Ângelo Hank <hankangelo6@gmail.com>
2026-09-02 22:20:17 -0300

    docs:  listando funcionalidades do sistema

--- minha-primeira-api/index.js
+++ minha-primeira-api/index.js
@@ -2,13 +2,43 @@ import express from "express";
 
 const app = express(); //primeiro pilar: instancia do express
 
-app.get("/", (req, res) => {
+/**
+ * idLivro -> identificador / int
+ * dsTitulo -> string
+ * dsAutor -> string
+ * fgDisponivel -> boolean
+ */
+
+let livros = []; //banco de dados
+
+//rota:
+//metodo + caminho + funcao
+app.get("/", function (req, res) {
+  //rota raiz
   res.send("seja bem vindo à gestao de livros");
 });
 
 app.get("/livros", function (pergunta, resposta) {
   //segundo pilar: rotas
-  resposta.send("Hello world!");
+  resposta.send("hello world");
 });
 
 app.listen(3000); //terceiro pilar: porta a ser ouvida
+
+/*
+cadastrar livros
+  POST
+
+buscar todos livros
+buscar um livro pelo nome
+buscar um livro pelo id
+  GET
+
+emprestar livro
+devolver livro
+  PUT/PATCH
+
+
+deletar livro
+  DELETE
+*/
