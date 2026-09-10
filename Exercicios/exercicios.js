505d7c50ac92ac0502e8b531d4accc762a537890
Cleiton de Quadros <cleitondequadros2@gmail.com>
2026-09-09 21:48:31 -0300

    feat: Exercicio aula 09-09-2026

--- Exercicios/exercicios.js
+++ Exercicios/exercicios.js
@@ -23,5 +23,5 @@ for (let i = 0; i < livros.length; i++) {
 const livro_mais_novo = livros.find((livro) => livro.ano > 2000);
 console.log(livro_mais_novo.titulo);
 
-const posicao_livro_mais_novo = livros.findIndex((livro) => livro.ano > 2000);
+const posicao_livro_mais_novo = livros.findIndex((livro) => livro.ano < 1950);
 console.log(posicao_livro_mais_novo);
