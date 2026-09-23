b59a7b63e8a8a242a409e6a474140c9b4229a7ef
Ângelo Hank <hankangelo6@gmail.com>
2026-09-23 20:24:08 -0300

    feat: criando rota de post

--- minha-primeira-api/index.js
+++ minha-primeira-api/index.js
@@ -1,14 +1,14 @@
 import express from "express";
 
 const app = express(); //primeiro pilar: instancia do express
-
+app.use(express.json());
 /**
  * idLivro -> identificador / int
  * dsTitulo -> string
  * dsAutor -> string
  * fgDisponivel -> boolean
  */
-
+let ultimo_id = 1;
 let livros = [
   {
     idLivro: 1,
@@ -23,6 +23,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/livros", function (req, res) {
+  console.log("chamando rota GET /livros");
   res.json(livros);
 });
 
@@ -30,10 +31,46 @@ app.get("/livros/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
   if (isNaN(id)) {
+    //se nao for um numero
     return res
-      .status(400)
+      .status(400) //requisicao mal formada
       .json({ mensagem: "o parametro precisa ser um numero valido" });
   }
+
+  let livro = livros.find((livro) => {
+    return livro.idLivro === id;
+  });
+
+  if (!livro) {
+    return res.status(404).send();
+  }
+
+  res.json(livro);
+});
+
+app.post("/livros", (req, res) => {
+  let autor_enviado = req.body.dsAutor;
+  let titulo_enviado = req.body.dsTitulo;
+
+  if (!autor_enviado || !titulo_enviado) {
+    return res
+      .status(400)
+      .json({ mensagem: "dados faltando, verifique autor e titulo" });
+  }
+
+  let id_novo = ultimo_id + 1;
+  ultimo_id++;
+
+  let novo_livro = {
+    idLivro: id_novo,
+    fgDisponivel: true,
+    dsTitulo: titulo_enviado,
+    dsAutor: autor_enviado,
+  };
+
+  livros.push(novo_livro); //eu adicionei um novo livro ao " banco de dados"
+
+  res.status(201).json(novo_livro);
 });
 
 app.listen(3000); //terceiro pilar: porta a ser ouvida
@@ -43,7 +80,6 @@ cadastrar livros
   POST
 
 buscar todos livros
-buscar um livro pelo nome
 buscar um livro pelo id
   GET
 
